fff2b55eb4eb929b429eb91c7ac0a199da318c77
Jesse Thomas <jayayetea@gmail.com>
2017-03-07 21:33:59 -0600

    initalization and files

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+
+var config = {
+	apiKey: "AIzaSyAqziado4gyKYsw5xxje865_8NqyTS5GsA",
+	authDomain: "rps-game-2769e.firebaseapp.com",
+	databaseURL: "https://rps-game-2769e.firebaseio.com",
+	storageBucket: "rps-game-2769e.appspot.com",
+	messagingSenderId: "539255618061"
+};
+
+firebase.initializeApp(config);
